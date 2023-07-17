00b9daee5cd7460d725485bc4436eb96040348ae
jundachen10 <jundachen10@gmail.com>
2023-07-16 21:11:28 -0400

    plan

--- Program.cs
+++ Program.cs
@@ -2,9 +2,21 @@
 
 class Program
 {
-    static void Main(string[] args)
+    static void Main()
     {
-        Console.WriteLine("Hello, World!");
+        // Read the json file
+
+        // Parse the json data
+
+        // Create a new Excel package
+        
+            // Create a new worksheet
+        
+            // Write header row
+         
+            // Write data row
+           
+            // Save the Excel package
+       
     }
 }
-
